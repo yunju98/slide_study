06bc01b07f9404205e7991ea57e552f7ff869a18
yunju <psw05067@naver.com>
2023-07-26 18:03:13 +0900

    first commit

---
+++ js/index.js
@@ -0,0 +1,31 @@
+let positionLeft = 0;
+
+$(".btn_next").click((e) => {
+    if(positionLeft === 4){
+        e.preventDefault();
+    }else{
+        positionLeft++;
+    $(".gallery_list").css({
+        transition : "all ease 0.5s",
+        "margin-left" : positionLeft * -300 + "px",
+    });
+    }
+})
+
+$(".btn_back").click((e) => {
+    if(positionLeft === 0){
+        e.preventDefault();
+    }else{
+        positionLeft--;
+    $(".gallery_list").css({
+        transition : "all ease 0.5s",
+        "margin-left" : positionLeft * -300 + "px",
+    });
+    }
+})
+
+//슬라이드 다음, 이전버튼(button태그) 만들기
+//다음버튼 클릭시 -> 좌로 300px 이동 e.preventDefault(); 정지
+//이전버튼 클릭시 -> 우로 300px 이동
+
+//메뉴 -> jQuery로 만들기
