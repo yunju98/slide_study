194b2acd67127ef05f7e174cdb2ddd1ae35035d9
yunju <psw05067@naver.com>
2023-07-29 17:22:57 +0900

    수정중

--- js/index.js
+++ js/index.js
@@ -1,5 +1,29 @@
 let positionLeft = 0;
 
+
+$(".pages > li > button").click(function(){
+    $(this).addClass("active");
+    let inx = $(this).parent().index();
+    $(".gallery_list").css({
+        transition : "all ease 0.5s",
+        "margin-left" :inx * -300 + "px",
+    });
+
+    $(this).blur(function(){
+        $(this).removeClass("active");
+    })
+    if(inx === 0){
+        $(".btn_back").addClass("gray")
+    }else{
+        $(".btn_back").removeClass("gray")
+    }
+    if(inx === 4){
+        $(".btn_next").addClass("gray")
+    }else{
+        $(".btn_next").removeClass("gray")
+    }
+})
+
 $(".btn_next").click((e) => {
     if(positionLeft === 4){
         e.preventDefault();
